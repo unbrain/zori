d03b02322151e1611ab5caaf626a67776fb1727a
Mars <marsorsun@gmail.com>
2018-08-31 21:34:32 +0800

    mounted check button-group child

--- src/button-group.vue
+++ src/button-group.vue
@@ -1,26 +1,34 @@
 <template>
     <div class="g-button-group">
         <slot>
-
         </slot>
     </div>
 </template>
 <script>
-    export default {}
+    export default {
+        mounted() {
+            for(let node of this.$el.children){
+                let name = node.nodeName.toLowerCase()
+                if(name !== 'button'){
+                    console.warn('button-group childNode must be button')
+                }
+            }
+        }
+    }
 </script>
 <style lang="scss">
     .g-button-group {
         display: inline-flex;
         > .g-button {
-            &:hover{
+            &:hover {
                 z-index: 1;
             }
             margin-left: -1px;
-            &:first-child{
+            &:first-child {
                 border-bottom-left-radius: var(--border-radius);
                 border-top-left-radius: var(--border-radius);
             }
-            &:last-child{
+            &:last-child {
                 border-top-right-radius: var(--border-radius);
                 border-bottom-right-radius: var(--border-radius);
             }
