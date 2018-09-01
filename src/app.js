5839c63dd94316f87057b926bddaaac26e81c12c
Mars <marsorsun@gmail.com>
2018-09-01 10:03:59 +0800

    use chai

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 
+
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
@@ -13,3 +14,61 @@ new Vue({
         isloading: false,
     }
 })
+
+import Chai from 'chai'
+const expect = Chai.expect
+{
+    const Constuctor = Vue.extend(Button)
+    const button = new Constuctor({
+        propsData: {
+            icon: 'setup'
+        }
+    })
+    button.$mount()
+    let use = button.$el.querySelector('use')
+    expect(use.getAttribute('xlink:href')).to.eq('#icon-setup')
+}
+{
+    const Constuctor = Vue.extend(Button)
+    const button = new Constuctor({
+        propsData: {
+            loading: true
+        }
+    })
+    button.$mount()
+    let use = button.$el.querySelector('use')
+    expect(use.getAttribute('xlink:href')).to.eq('#icon-loading')
+}
+{
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constuctor = Vue.extend(Button)
+    const button = new Constuctor({
+        propsData: {
+            loading: true
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    let div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constuctor = Vue.extend(Button)
+    const button = new Constuctor({
+        propsData: {
+            loading: true,
+            iconPosition: 'right'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
