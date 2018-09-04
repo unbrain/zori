674dff47e6b474a6faa1214af8db7b34ee645343
Mars <marsorsun@gmail.com>
2018-09-04 16:52:25 +0800

    col.vue offset

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col" :class="span &&`span-${span}`">
+  <div class="col" :class="[span &&`span-${span}`, offset && `offset-${offset}`]">
     <slot></slot>
   </div>
 </template>
@@ -11,6 +11,9 @@
       span: {
         type: [String, Number],
       },
+      offset: {
+        type: [String, Number]
+      }
     },
   }
 </script>
@@ -26,6 +29,12 @@
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n/24)*100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n}{
+        margin-left: ($n/24)*100%;
       }
     }
   }
