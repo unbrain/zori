f4f24b20c4c453fee3b7b5f1326841d2f2a08e72
Mars <marsorsun@gmail.com>
2018-09-06 16:52:56 +0800

    responsive done

--- src/col.vue
+++ src/col.vue
@@ -5,6 +5,16 @@
 </template>
 
 <script>
+  let validator = (value) => {
+    let keys = Object.keys(value)
+    let valid = true
+    keys.forEach(key => {
+      if (!['span', 'offset'].includes(key)) {
+        valid = false
+      }
+    })
+    return valid
+  }
   export default {
     name: 'ZoriCol',
     props: {
@@ -14,6 +24,10 @@
       offset: {
         type: [String, Number],
       },
+      ipad: {type: Object, validator},
+      narrowPc: {type: Object, validator},
+      pc: {type: Object, validator},
+      widePc: {type: Object, validator},
     },
     data () {
       return {
@@ -22,8 +36,13 @@
     },
     computed: {
       colClass () {
-        let {span, offset} = this
-        return [span && `span-${span}`, offset && `offset-${offset}`]
+        let {span, offset, ipad, narrowPc, pc, widePc} = this
+        return [
+          span && `span-${span}`, offset && `offset-${offset}`,
+          ... (ipad ? [`col-ipad-${ipad.span}`] : []),
+          ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
+          ... (pc ? [`col-pc-${pc.span}`] : []),
+          ... (widePc ? [`col-wide-pc-${widePc.span}`] : [])]
       },
       colStyle () {
         let {gutter} = this
@@ -46,6 +65,62 @@
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n/24)*100%;
+      }
+    }
+    @media (min-width: 577px) {
+      $class-prefix: col-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 769px) {
+      $class-prefix: col-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 993px){
+      $class-prefix: col-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    @media (min-width: 1201px) {
+      $class-prefix: col-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
       }
     }
   }
