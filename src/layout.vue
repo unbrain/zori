9e7730b37896f59ba2374ea9cd6e99cf95072ea6
Mars <marsorsun@gmail.com>
2018-09-08 18:26:41 +0800

    layout done

---
+++ src/layout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="layout" :class="layoutClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ZoriLayout',
+    data () {
+      return {
+        layoutClass: {
+          hasSider: false,
+        },
+      }
+    },
+    mounted () {
+      this.$children.forEach(vm => {if (vm.$options.name === 'ZoriSider') {this.layoutClass.hasSider = true}})
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+  .layout {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    &.hasSider {
+      flex-direction: row;
+    }
+  }
+</style>
---
+++ src/sider.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ZoriSider',
+  }
+</script>
+
+<style scoped>
+
+</style>
