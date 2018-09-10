6c84a64d08d1986418f3f4affe27624ce93c739d
Mars <marsorsun@gmail.com>
2018-09-10 14:38:00 +0800

    append toast plugin

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Content from  './content'
 import Footer from './footer'
 import Sider from './sider'
 import Layout from './layout'
+import Toast from './toast'
+import plugin from './plugin'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -22,6 +24,9 @@ Vue.component('g-content', Content)
 Vue.component('g-footer', Footer)
 Vue.component('g-sider', Sider)
 Vue.component('g-layout', Layout)
+Vue.component('g-toast', Toast)
+Vue.use(plugin)
+
 new Vue({
   el: '#app',
   data: {
@@ -33,4 +38,7 @@ new Vue({
       console.log(e)
     },
   },
+  created(){
+    this.$toast('nihao')
+  }
 })
---
+++ src/toast.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'toast',
+  }
+</script>
+
+<style scoped lang="scss">
+  $font-size: 14px;
+  $toast-bg: rgba(0, 0, 0, 0.75);
+  $border-radius: 3px;
+  .toast {
+    position: fixed;display: flex;align-items: center;padding: 0.5em .75em;margin: .75em 0;top: 0;left: 50%;transform: translateX(-50%);color: white;font-size: $font-size;
+    background-color: $toast-bg;box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);border-radius: $border-radius;
+  }
+</style>
